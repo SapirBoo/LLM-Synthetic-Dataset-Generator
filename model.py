c1565095bedd7da8cf75e71b5d3b19836900348e
SapirBoo <sapirbuz5@gmail.com>
2026-07-13 16:52:23 +0300

    Update comment for model loading function

---
+++ model.py
@@ -0,0 +1,15 @@
+# define helper function to load model and tokenizer - include cache
+from functools import lru_cache
+
+@lru_cache(maxsize=4)
+def load_model(model_name):
+  quant_config = BitsAndBytesConfig(
+      load_in_4bit=True,
+      bnb_4bit_use_double_quant=True,
+      bnb_4bit_compute_dtype=torch.bfloat16,
+      bnb_4bit_quant_type="nf4"
+  )
+  tokenizer = AutoTokenizer.from_pretrained(model_name)
+  tokenizer.pad_token = tokenizer.eos_token
+  model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", quantization_config=quant_config)
+  return tokenizer,model
