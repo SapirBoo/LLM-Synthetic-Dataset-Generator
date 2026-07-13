a696c979a1db72d5b88a230b7ed523fabb802bdf
SapirBoo <sapirbuz5@gmail.com>
2026-07-13 16:50:00 +0300

    Add data formatting function to process JSON

---
+++ formatter.py
@@ -0,0 +1,16 @@
+def format_data(data):
+  content = re.sub(r"<think>.*?</think>", "",data,
+    flags=re.DOTALL).strip()
+
+  #Find start of the Json 
+  start = content.find("[")
+  if start == -1:
+    raise ValueError("No JSON found")
+
+  content = content[start:]
+  
+  decoder = json.JSONDecoder()
+  new_data, _ = decoder.raw_decode(content)
+
+
+  return pd.DataFrame(new_data)
